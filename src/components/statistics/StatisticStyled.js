987927dc6dd1400359dbc958c489131d29daeb83
Taras Prystuplyk <1988taras@ukr.net>
2022-08-14 22:57:16 +0300

    start

---
+++ src/components/statistics/StatisticStyled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Items = styled.li`
+  margin-top: ${p => p.theme.space[0]}px;
+  margin-bottom: ${p => p.theme.space[2]}px;
+`;
+export const ItemsText = styled.p`
+  font-size: ${p => p.theme.fontSizes.l};
+  color: ${p => p.theme.colors.secondary};
+  margin-top: ${p => p.theme.space[0]}px;
+  margin-bottom: ${p => p.theme.space[2]}px;
+`;
+
+export const TotalResults = styled.p`
+  font-size: ${p => p.theme.fontSizes.l};
+  color: ${p => p.theme.colors.red};
+  margin-top: ${p => p.theme.space[0]}px;
+  margin-bottom: ${p => p.theme.space[2]}px;
+`;
